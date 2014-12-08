a639c24af62d86dd2b4c4d5cb00b86f30163b611
John Pettigrew <john.pettigrew404@gmail.com>
2014-12-07 23:28:46 -0600

    Style changes; show saved ip address when restarted.

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   var buttonPress = function(button){
-    console.log('test');
+    /*
+      Function to send command to Roku device.
+    */
     var ipAddress = $('#address').val();
     $.post('http://'+ipAddress+':8060/keypress/'+button, function(){})
     .fail(function(){
-      console.log(ipAddress);
       toast('error connecting to \''+ipAddress+'\'', 1500);
     });
   }
+
+  //Events
   $('#left-button').on('click', function(){
     buttonPress('left');
   });
@@ -31,26 +34,37 @@ $(document).ready(function(){
     if(!$('#address').is(':focus')){
       switch(e.which){
         case 13:
-          console.log('select');
           buttonPress('select');
           break;
         case 37:
-          console.log('left');
           buttonPress('left');
           break;
         case 38:
-          console.log('up');
           buttonPress('up');
           break;
         case 39:
-          console.log('right');
           buttonPress('right');
           break;
         case 40:
-          console.log('down');
           buttonPress('down');
           break;
       }
     }
-  })
+  });
+
+  //Save the IP address when the user is done entering it.
+  $('#address').focusout(function(){
+    chrome.storage.local.set({'remoteIpAddress': $('#address').val()});
+  });
+
+  //setup tabs
+  $('.tabs').tabs();
+
+  //try to set saved ip address
+  chrome.storage.local.get('remoteIpAddress', function(data){
+    $('#address').val(data.remoteIpAddress);
+    if(data.remoteIpAddress.length > 0){
+      $('#address-label').addClass('active');
+    }
+  });
 });
