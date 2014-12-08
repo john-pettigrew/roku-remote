58061d740b51bb58cd519f739a03df1bfa4e6c0f
John Pettigrew <john.pettigrew404@gmail.com>
2014-12-08 01:49:15 -0600

    Populate app list with channels; add ability to open app from app list.

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -66,5 +66,49 @@ $(document).ready(function(){
     if(data.remoteIpAddress.length > 0){
       $('#address-label').addClass('active');
     }
+    getChannels();
   });
+
+  //get channel list
+  var getChannels = function(){
+    var goToChannel = function(id){
+        /*
+          Function to switch channels based on given id
+        */
+        var ipAddress = $('#address').val();
+        $.post('http://'+ipAddress+':8060/launch/'+id, function(){})
+        .fail(function(){
+          toast('error connecting to \''+ipAddress+'\'', 1500);
+        });
+    }
+
+    var ipAddress = $('#address').val();
+    if(ipAddress !== undefined && ipAddress.length > 0){
+      $.get('http://'+ipAddress+':8060/query/apps',
+      function(data){
+        var appsArray = $(data).find('app');
+        var count = 0;
+        appsArray.each(function(value){
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', 'http://'+ipAddress+':8060/query/icon/'+$(appsArray[value]).attr('id'), true);
+          xhr.responseType = 'blob';
+          xhr.onload = function(e){
+            $('#channel-list').append('<div class="channel-item" channelId="'+$(appsArray[value]).attr('id')+'"><img src="'+window.URL.createObjectURL(this.response)+'" channelId="'+$(appsArray[value]).attr('id')+'"></img><li channelId="'+$(appsArray[value]).attr('id')+'">'+$(appsArray[value]).text()+'&nbsp;</li></div>');
+            count++;
+            if(count === appsArray.length){
+              $('.channel-item').on('click', function(e){
+                goToChannel(e.target.attributes.channelid.value);
+              });
+            }
+          };
+          xhr.send();
+        });
+      },
+      'xml')
+      .fail(function(){
+        toast('error connecting to \''+ipAddress+'\'', 1500);
+      });
+    }
+  }
+
 });
